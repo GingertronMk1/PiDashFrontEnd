7d8bb64b3c00b6d10ab43cfe323884351742bb04
Jack Ellis <jackellis1504@gmail.com>
2022-04-13 17:54:43 +0100

    Added template widget

---
+++ src/components/_template.vue
@@ -0,0 +1,16 @@
+<script setup>
+import WidgetTemplate from "@/templates/WidgetTemplate.vue";
+import { computed } from "vue";
+
+const props = defineProps({
+  data: {
+    type: [null, Array],
+    default: null,
+  },
+});
+</script>
+<template>
+  <WidgetTemplate v-if="data"> </WidgetTemplate>
+</template>
+
+<style lang="scss"></style>
