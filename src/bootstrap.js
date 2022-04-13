d5fc8bcf87ced58ca191af574c7b238736201f07
Jack Ellis <jackellis1504@gmail.com>
2022-04-13 16:48:09 +0100

    Using env vars

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,6 +1,12 @@
 window.axios = require("axios");
 
-window.axios.defaults.baseURL = "http://192.168.86.177:5000";
+console.table(process.env);
+
+window.axios.defaults.baseURL = [
+  "http://",
+  process.env.VUE_APP_PI_URL ?? "0.0.0.0",
+  ":5000",
+].join("");
 
 window.bytesToMegabytes = function (bytes) {
   return (Number.parseInt(bytes) / 1024 / 1024).toFixed(2);
