90b443f5347111b051d0424bc7b4c57573b580b6
Jack Ellis <jackellis1504@gmail.com>
2022-04-20 21:51:18 +0100

    Using better formatting fn

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,12 +4,29 @@ window.axios.defaults.baseURL = `${process.env.VUE_APP_PI_URL ?? ""}/dash-api`;
 
 window.axios.defaults.port = "5000";
 
+window.bytesToOther = function (bytes, unit = "G") {
+  let exponent = 0;
+  switch (unit) {
+    case "P":
+      exponent++;
+    case "T":
+      exponent++;
+    case "G":
+      exponent++;
+    case "M":
+      exponent++;
+    case "K":
+      exponent++;
+  }
+  return (Number.parseInt(bytes) / Math.pow(1024, exponent)).toFixed(2);
+};
+
 window.bytesToMegabytes = function (bytes) {
-  return (Number.parseInt(bytes) / 1024 / 1024).toFixed(2);
+  return bytesToOther(bytes, "M");
 };
 
 window.bytesToGigabytes = function (bytes) {
-  return (Number.parseFloat(bytesToMegabytes(bytes)) / 1024).toFixed(2);
+  return bytesToOther(bytes, "G");
 };
 
 window.initialiseWidget = function (
