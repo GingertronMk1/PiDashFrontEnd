40e80b80bfb4d14e9c84aaa1f5f3201ff614d214
Jack Ellis <jackellis1504@gmail.com>
2022-04-23 12:08:37 +0100

    Fix ma defaults

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,14 +1,11 @@
 window.axios = require("axios");
 
-window.axios.defaults = {
-  ...window.axios.defaults,
-  baseURL: process.env.VUE_APP_PI_URL ?? "",
-  paramsSerializer: (params) => {
-    console.table(params);
-    return Object.entries(params)
-      .map(([key, value]) => `${key}=${JSON.stringify(value)}`)
-      .join("&");
-  },
+window.axios.defaults.baseURL = process.env.VUE_APP_PI_URL ?? "";
+window.axios.defaults.paramsSerializer = (params) => {
+  console.table(params);
+  return Object.entries(params)
+    .map(([key, value]) => `${key}=${JSON.stringify(value)}`)
+    .join("&");
 };
 
 function getBaseLog(base, value) {
