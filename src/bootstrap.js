c20e4f60508b886b8667f4c02987aa5ca9ceef59
Jack Ellis <jackellis1504@gmail.com>
2022-04-26 16:00:27 +0100

    Remove unused functions

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -12,28 +12,16 @@ window.axios.defaults.paramsSerializer = (params) => {
     .join("&");
 };
 
-function getBaseLog(base, value) {
-  return Math.log(value) / Math.log(base);
-}
-
 window.bytesToOther = function (bytes) {
   let amount = Number.parseInt(bytes);
   if (amount === 0) return "0B";
   prefixIndex = 0;
   const prefixes = ["", "K", "M", "G", "T", "P"];
   const base = 1024;
-  const log = getBaseLog(base, amount);
+  const log = Math.log(amount) / Math.log(base);
   const logFloored = Math.floor(log);
 
   return (amount / base ** logFloored).toFixed(2) + prefixes[logFloored] + "B";
-};
-
-window.bytesToMegabytes = function (bytes) {
-  return bytesToOther(bytes, "M");
-};
-
-window.bytesToGigabytes = function (bytes) {
-  return bytesToOther(bytes, "G");
 };
 
 window.initialiseWidget = function (
