246713d438b3fb99ca0d5b061e90e0bdf965484f
Jack Ellis <jackellis1504@gmail.com>
2022-05-12 08:08:29 +0100

    Moved stuff from main to symbols

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { InitialiseWidgetKey, AxiosKey, BytesToOtherKey } from "./symbols";
+import Symbols from "./symbols";
 import "./assets/style.css";
 
-const $axios = require("axios");
-$axios.defaults.baseURL = process.env.VUE_APP_PI_URL ?? "";
-$axios.defaults.paramsSerializer = (params: object) => {
-  return Object.entries(params)
-    .map(([key, value]) => {
-      if (Array.isArray(value)) {
-        return value.map((v) => `${key}=${v}`).join("&");
-      }
-      return `${key}=${value}`;
-    })
-    .join("&");
-};
-
-function bytesToOther(bytes: number): string {
-  if (bytes === 0) return "0B";
-  const prefixes = ["", "K", "M", "G", "T", "P"];
-  const base = 1024;
-  const log = Math.log(bytes) / Math.log(base);
-  const logFloored = Math.floor(log);
-
-  return (bytes / base ** logFloored).toFixed(2) + prefixes[logFloored] + "B";
-}
-
-function initialiseWidget(
-  callback: Function,
-  interval: number = 1000,
-  stopOnLostFocus: boolean = true
-) {
-  callback();
-  setInterval(() => {
-    if (document.hasFocus() || !stopOnLostFocus) {
-      callback();
-    }
-  }, interval);
-}
-
 createApp(App)
-  .provide(InitialiseWidgetKey, initialiseWidget)
-  .provide(BytesToOtherKey, bytesToOther)
-  .provide(AxiosKey, $axios)
+  .provide(Symbols.InitialiseWidgetKey, Symbols.initialiseWidget)
+  .provide(Symbols.BytesToOtherKey, Symbols.bytesToOther)
+  .provide(Symbols.AxiosKey, Symbols.axios)
   .mount("#app");
--- src/symbols.ts
+++ src/symbols.ts
@@ -1,11 +1,56 @@
 import { InjectionKey } from "vue";
 import { AxiosInstance } from "axios";
 
-export const InitialiseWidgetKey: InjectionKey<
+const AxiosKey: InjectionKey<AxiosInstance> = Symbol("AxiosKey");
+
+const axios = require("axios");
+axios.defaults.baseURL = process.env.VUE_APP_PI_URL ?? "";
+axios.defaults.paramsSerializer = (params: object) => {
+  return Object.entries(params)
+    .map(([key, value]) => {
+      if (Array.isArray(value)) {
+        return value.map((v) => `${key}=${v}`).join("&");
+      }
+      return `${key}=${value}`;
+    })
+    .join("&");
+};
+
+const BytesToOtherKey: InjectionKey<(bytes: number) => string> =
+  Symbol("BytesToOtherKey");
+
+function bytesToOther(bytes: number): string {
+  if (bytes === 0) return "0B";
+  const prefixes = ["", "K", "M", "G", "T", "P"];
+  const base = 1024;
+  const log = Math.log(bytes) / Math.log(base);
+  const logFloored = Math.floor(log);
+
+  return (bytes / base ** logFloored).toFixed(2) + prefixes[logFloored] + "B";
+}
+
+const InitialiseWidgetKey: InjectionKey<
   (callback: Function, interval?: number, stopOnLostFocus?: boolean) => void
 > = Symbol("InitialiseWidgetKey");
 
-export const AxiosKey: InjectionKey<AxiosInstance> = Symbol("AxiosKey");
+function initialiseWidget(
+  callback: Function,
+  interval: number = 1000,
+  stopOnLostFocus: boolean = true
+) {
+  callback();
+  setInterval(() => {
+    if (document.hasFocus() || !stopOnLostFocus) {
+      callback();
+    }
+  }, interval);
+}
 
-export const BytesToOtherKey: InjectionKey<(bytes: number) => string> =
-  Symbol("BytesToOtherKey");
+export default {
+  axios,
+  bytesToOther,
+  initialiseWidget,
+  InitialiseWidgetKey,
+  AxiosKey,
+  BytesToOtherKey,
+};
